de7c72f7b622135c48020f0dea0d224c3904d3bd
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-10 12:53:35 +0530

    fix(ts): update session types and resolve env strict type errors

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET!,
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,19 +1,20 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import mongoose from "mongoose";
 
-// Establish MongoDB connection
 const connectToDatabase = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URI);
-        const connectionDetails = `${connection.connection.host}:${connection.connection.port}/${connection.connection.name}`;
-
-        console.log(`MongoDB connected: ${connectionDetails}`);
-    } catch (error) {
-        console.error(`Database connection error: ${(error as Error).message}`);
-        process.exit(1);
+  try {
+    const mongoUri = process.env.MONGO_URI;
+    if (!mongoUri) {
+      throw new Error("MONGO_URI environment variable is not defined.");
     }
+
+    const connection = await mongoose.connect(mongoUri);
+    const connectionDetails = `${connection.connection.host}:${connection.connection.port}/${connection.connection.name}`;
+
+    console.log(`MongoDB connected: ${connectionDetails}`);
+  } catch (error) {
+    console.error(`Database connection error: ${(error as Error).message}`);
+    process.exit(1);
+  }
 };
 
 export default connectToDatabase;
